f76dc02d3468aa47071452c513b0be923d436340
Cristina-Pe <cristinaperezmeneses@hotmail.com>
2021-11-02 10:49:46 +0100

    db

--- DB/db.sql
+++ DB/db.sql
@@ -13,9 +13,14 @@ primary key(id)
 );
 
 insert into mascota values
-(1, 'Caniche', 'perro','M', 2, '12/02/2019'),
-(2, 'Dalmata', 'perro','M', 3, '12/02/2018'),
-(3, 'Golden', 'perro','F', 4, '12/02/2017');
+(1, 'Golden', 'mamifero','M', 2, '12/02/2019'),
+(2, 'Caniche', 'mamifero','M', 3, '02/12/2018'),
+(3, 'Siames', 'mamifero','F', 2, '19/04/2019'),
+(4, 'Rana', 'reptil','M', 1, '12/02/2020'),
+(5, 'Loro', 'ave','M', 3, '12/02/2018'),
+(6, 'Serpiente', 'reptil','F', 5, '12/02/2016'),
+(7, 'Hámster', 'mamifero','M', 2, '04/03/2019')
+
 
 select*from mascota;
 
